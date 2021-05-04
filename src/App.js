29eb8030c5db950dbce00399d8cccb882c9e3fc4
SterlingOrth <71182922+SterlingOrth@users.noreply.github.com>
2021-05-03 20:00:03 -0600

    testing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoList from "./TodoList"
 
 function App() {
+  const [todos, setTodos] = useState(['Todos 1', 'Todos 2'])
   return (
     <>
-  <TodoList />
+  <TodoList todos={todos}/>
   <input type="text" />
+  <button>Add Todo</button>
+  <button>Clear Completed Todos</button>
+  <div>0 Left to do</div>
   </>
   )
 }
